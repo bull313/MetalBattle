e456118458d68f102cdf8a20cad6946388e62a1f
bull313 <bladick@asu.edu>
2020-10-30 14:35:53 -0700

    Set deployment configuration
    Added DB connection vars
    Added composer JSON

--- info.php
+++ info.php
@@ -1,8 +1,9 @@
 <?php
-  define('NET', 'localhost');
-  define('USER', 'root');
-  define('PWD', '');
-  define('DB', 'metalbattle');
+  define('NET', getenv('DB_HOST'));
+  define('USER', getenv('DB_USER'));
+  define('PWD', getenv('DB_PWD'));
+  define('DB', getenv('DB'));
+  define('PORT', getenv('DB_PORT'));
   define('Q_FAIL', "<h2>MYSQL had a problem running a query....</h2>");
   define('FAIL', "<h2>Error connecting to database " . DB . ".</h2>");
   define('COMP_SIZE', 16);
